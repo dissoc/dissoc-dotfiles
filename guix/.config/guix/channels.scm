c659ed21b4fc401db51e37c3ddd0412f1936a235
Justin Bishop <mail@dissoc.me>
2024-02-19 14:51:56 -0600

    added sops-guix to channels

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -36,4 +36,13 @@
          (make-channel-introduction
           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
-           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (channel
+        (name 'sops-guix)
+        (url "https://git.sr.ht/~fishinthecalculator/sops-guix")
+        (branch "main")
+        (introduction
+         (make-channel-introduction
+          "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+          (openpgp-fingerprint
+           "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))))
